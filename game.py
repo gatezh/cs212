ac38ad59f060b2fb004a4e9710f3ddd55900efd0
Wonderbird <gatezh@users.noreply.github.com>
2016-08-02 17:23:54 -0400

    test: Add tests for card_rank func Changes to be committed: 	modified:   game.py

--- game.py
+++ game.py
@@ -44,6 +44,9 @@ def test():
     sf = "6C 7C 8C 9C TC".split() # Straight Flush
     fk = "9D 9H 9S 9C 7D".split() # Four of a Kind
     fh = "TD TC TH 7C 7D".split() # Full House
+    assert card_ranks(sf) == [10, 9, 8, 7, 6]
+    assert card_ranks(fk) == [9, 9, 9, 9, 7]
+    assert card_ranks(fh) == [10, 10, 10, 7, 7]
     assert poker([sf, fk, fh]) == sf
     assert poker([fk, fh]) == fk
     assert poker([fh, fh]) == fh
@@ -52,5 +55,6 @@ def test():
     assert hand_rank(sf) == (8, 10)
     assert hand_rank(fk) == (7, 9, 7)
     assert hand_rank(fh) == (6, 10, 7)
+    return 'tests pass'
 
 print test()
