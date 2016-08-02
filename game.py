bf947ea071af713a90aa07c6f473e6b50f3898b2
Wonderbird <gatezh@users.noreply.github.com>
2016-08-02 12:12:21 -0400

    test: Add more tests Changes to be committed: 	modified:   game.py

--- game.py
+++ game.py
@@ -11,6 +11,8 @@ def test():
 	fk = "9D 9H 9S 9C 7D".split()
 	fh = "TD TC TH 7C 7D".split()
 	assert poker([sf, fk, fh]) == sf
+	assert poker([fk, fh]) == fk
+    assert poker([fh, fh]) == fh
 	return "tests pass"
 
 print test()
