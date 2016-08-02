e84398b84db993762fbf808d9052172781cbbb90
Wonderbird <gatezh@users.noreply.github.com>
2016-08-02 17:14:36 -0400

    test: Add more test to test function Changes to be committed: 	modified:   game.py

--- game.py
+++ game.py
@@ -7,14 +7,16 @@ def hand_rank(hand):
 
 def test():
     "Test cases for the functions in poker program"
-    sf = "6C 7C 8C 9C TC".split() 
-    fk = "9D 9H 9S 9C 7D".split() 
-    fh = "TD TC TH 7C 7D".split()
+    sf = "6C 7C 8C 9C TC".split() # Straight Flush
+    fk = "9D 9H 9S 9C 7D".split() # Four of a Kind
+    fh = "TD TC TH 7C 7D".split() # Full House
     assert poker([sf, fk, fh]) == sf
     assert poker([fk, fh]) == fk
     assert poker([fh, fh]) == fh
-    assert poker([fh]) == fh
-    assert poker([fk] + 99 * [fh]) == fk
-    return 'tests pass'
+    assert poker([sf]) == sf
+    assert poker([sf] + 99*[fh]) == sf
+    assert hand_rank(sf) == (8, 10)
+    assert hand_rank(fk) == (7, 9, 7)
+    assert hand_rank(fh) == (6, 10, 7)
 
 print test()
