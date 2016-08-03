2550f77c8a79cc5a37928f9a4e68c36c67df3d27
Wonderbird <gatezh@users.noreply.github.com>
2016-08-03 17:48:21 -0400

    feat: Add allmax function to return list if a tie

--- game.py
+++ game.py
@@ -16,7 +16,19 @@
 
 def poker(hands):
     "Return the best hand: poker([hand,...] => hand)"
-    return max(hands, key=hand_rank)
+    return allmax(hands, key=hand_rank)
+
+def allmax(iterable, key=None):
+    "Return a list of all items equal to the max of the iterable."
+    result, maxval = [], None
+    key = key or (lambda x: x)
+    for x in iterable:
+        xval = key(x)
+        if not result or xval > maxval:
+            result, maxval = [x], xval
+        elif xval == maxval:
+            result.append(x)
+    return result
 
 def hand_rank(hand):
     ranks = card_ranks(hand)
@@ -44,7 +56,7 @@ def card_ranks(cards):
     ranks = ["--23456789TJQKA".index(r) for r,s in cards]
     ranks.sort(reverse=True)
     return [5, 4, 3, 2, 1] if (ranks == [14, 5, 4, 3, 2]) else ranks
-    
+
 def straight(ranks):
     "Return True if the ordered ranks form a 5-card straight."
     count = 0
