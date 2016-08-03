f4d12e706ed97d2d2ee605007f8a90c55d9c7d99
Wonderbird <gatezh@users.noreply.github.com>
2016-08-03 16:25:53 -0400

    feat: Add correction to card_ranks func to recognize Ace as 1

--- game.py
+++ game.py
@@ -43,7 +43,7 @@ def card_ranks(cards):
     "Return a list of the ranks, sorted with higher first."
     ranks = ["--23456789TJQKA".index(r) for r,s in cards]
     ranks.sort(reverse=True)
-    return ranks
+    return [5, 4, 3, 2, 1] if (ranks == [14, 5, 4, 3, 2]) else ranks
 
 def straight(ranks):
     "Return True if the ordered ranks form a 5-card straight."
