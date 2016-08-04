32bd4fe7d900de1c3d9096f95894a7ebb6101231
Wonderbird <gatezh@users.noreply.github.com>
2016-08-04 17:24:56 -0400

    feat: Add best_hand func to get the best hand out of 7 cards

--- game.py
+++ game.py
@@ -109,3 +109,21 @@ def test():
     return 'tests pass'
 
 print test()
+
+## Poker Homework 1, problem 1: 7-card stud
+## (hint: consider itertools)
+
+def best_hand(hand):
+    "From a 7-card hand, return the best 5 card hand."
+    return max(itertools.combinations(hand, 5), key=hand_rank)
+
+def test_best_hand():
+    assert (sorted(best_hand("6C 7C 8C 9C TC 5C JS".split()))
+            == ['6C', '7C', '8C', '9C', 'TC'])
+    assert (sorted(best_hand("TD TC TH 7C 7D 8C 8S".split()))
+            == ['8C', '8S', 'TC', 'TD', 'TH'])
+    assert (sorted(best_hand("JD TC TH 7C 7D 7S 7H".split()))
+            == ['7C', '7D', '7H', '7S', 'JD'])
+    return 'test_best_hand passes'
+
+print test_best_hand()
