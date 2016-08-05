0c0a94c87be5c7f0889dd6bda2c03198b7830e70
Wonderbird <gatezh@users.noreply.github.com>
2016-08-05 08:18:58 -0400

    Change file structure, add new func examples Changes to be committed: 	new file:   Knuth's Algorithm P..py 	renamed:    ../game.py -> game.py 	new file:   hand_frequencies.py

---
+++ Lesson_1/Knuth's Algorithm P..py
@@ -0,0 +1,10 @@
+def shuffle(deck):
+    "Knuth's Algorithm P."
+    N = len(deck)
+    for i in range(N - 1):
+        swap(deck, i, random.randrange(i, N))
+
+def swap(deck, i, j):
+    "Swap elements i and j of a collection."
+    print 'swap', i, j
+    deck[i], deck[j] = deck[j], deck[i]
